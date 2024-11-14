fd250b9073444b29afa8c632294b8b50fadba8b5
NetAcad DEVASC <changeme@example.com>
2024-11-14 15:47:04 +0000

    NONE - CHG - Added switchers and routers output via for loop

--- devnet-src/python/for-devices.py
+++ devnet-src/python/for-devices.py
@@ -1,4 +1,14 @@
 devices=["R1","R2","R3","S1","S2"]
+switches=[]
+routers=[]
 for item in devices:
-     if "R" in item:
-         print(item)
+    if "R" in item:
+        routers.append(item)
+        print(item)
+for item in devices:
+    if "S" in item:
+        switches.append(item)
+        print(item)
+
+print(f"my routers are: {routers}")
+print(f"my switches are: {switches}")
