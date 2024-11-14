43c5341470f46d4b9bd502a3afc42e1640def74b
NetAcad DEVASC <changeme@example.com>
2024-11-14 14:47:31 +0000

    NONE: CHG - replace 4 variables with a single dictionary for better code reading

--- devnet-src/python/personal-info.py
+++ devnet-src/python/personal-info.py
@@ -1,6 +1,8 @@
-firstName = input("What is your first name? ")
-lastName = input("What is your last name? ")
-location = input("What is your location? ")
-age = input("What is your age? ")
+personal_info = {}
 
-print(f"Hi {firstName} {lastName}! Your location is {location} and you are  {age} old!")
+personal_info["firstName"] = input("What is your first name? ")
+personal_info["lastName"] = input("What is your last name? ")
+personal_info["location"] = input("What is your location? ")
+personal_info["age"] = input("What is your age? ")
+
+print(f'Hi {personal_info["firstName"]} {personal_info["lastName"]}! Your location is {personal_info["location"]} and you are  {personal_info["age"]} old!')
