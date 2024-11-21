20a496fb66b357ecd537a8b16e1884b0e9930e47
NetAcad DEVASC <changeme@example.com>
2024-11-21 14:51:29 +0000

    NONE: UPD - added new layer 3 switch

--- devnet-src/python/for-devices.py
+++ devnet-src/python/for-devices.py
@@ -1,13 +1,13 @@
-devices=["R1","R2","R3","S1","S2"]
+devices=["R1","R2","R3","S1","S2","L3S1"]
 switches=[]
 routers=[]
 for item in devices:
-    if "R" in item:
-        routers.append(item)
-        print(item)
-    elif "S" in item:
+    if "S" in item:
         switches.append(item)
         print(item)
+    elif "R" in item:
+        routers.append(item)
+        print(item)
 
-print(f"my routers are: {routers}")
 print(f"my switches are: {switches}")
+print(f"my routers are: {routers}")
