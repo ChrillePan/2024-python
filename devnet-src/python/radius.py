ab4f04c49fae62dd36a715899da3ef11f6221169
Master C <aee23026@technikerschule-berlin.de>
2024-12-19 15:57:12 +0000

    NONE: CHG - Using classes

--- devnet-src/python/radius.py
+++ devnet-src/python/radius.py
@@ -1,18 +1,21 @@
 #! /usr/bin/ python3
 
-#List for my values
-radius_values = [2,5,7,9,11,13]
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
 
-def circumference(radius_values):
-  pi = 3.14159 
-  circumferenceValue = pi * radius_values * 2
-  return circumferenceValue
+    def circumference(self):
+        pi = 3.14159
+        circumferenceValue = pi * self.radius * 2
+        return circumferenceValue
 
-def printCircumference(radius_values):
-  myCircumference = circumference(radius_values)
-  print (f"Circumference of a circle with a radius of {radius_values:3} is {myCircumference:.2f}")
+    def printCircumference(self):
+        myCircumference = self.circumference()
+        print(f"Circumference of a circle with a radius of {self.radius:3} is {myCircumference:.2f}")
 
 #New Way
+#List for my values
+radius = [Circle(2),Circle(5),Circle(7)]
 
-for r in radius_values:
-    printCircumference(r)
+for r in radius:
+    r.printCircumference()
