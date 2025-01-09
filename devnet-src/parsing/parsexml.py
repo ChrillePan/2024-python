280bb98f23bbb8744540005b3a5ac903d4592345
Master C <aee23026@technikerschule-berlin.de>
2025-01-09 16:01:10 +0000

    NONE: ADD - Parsing with XML

---
+++ devnet-src/parsing/parsexml.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import xml.etree.ElementTree as ET
+import re
+
+myxml = ET.parse("myfile.xml")
+myroot = myxml.getroot()
+
+ns = re.match('{.*}',myroot.tag).group(0)
+editconf = myroot.find("{}edit-config".format(ns))
+defop = editconf.find("{}default-operation".format(ns))
+testop = editconf.find("{}test-option".format(ns))
+
+print("The default-operation contains: {}".format(defop.text))
+print("The test-option contains: {}".format(testop.text))
